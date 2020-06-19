cdd8da055e662319deb865fbae1828e0b39d6734
Your Name <you@example.com>
2020-06-19 13:53:51 -0400

    middleware added

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,28 @@
+let jwt = require('jsonwebtoken');
+let constants = require('../utils/constants');
+const { jwtSecret } = require('../utils/constants');
+
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  let token = req.headers.authorization; // grabs token from request headers property
+  let secret = constants.jwtSecret;
+
+  if(token) { 
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if(err) {
+        res.status(401).json({ message: "Invalid token" });
+      } else {
+        req.decodedToken = decodedToken; // creates a decoded token inside req
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ message: "valid credentials required" });
+  }
+
+  // res.status(401).json({ you: 'shall not pass!' });
 };
