82fdaf719ea30a86e2b72dfcaac02cae108cafed
Evelyn Nguyen <evelynnguyen@evelyns-mbp.lan>
2020-09-13 19:00:02 -0700

    done

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -5,10 +5,6 @@ require('dotenv').config({
     path: '.env'
 })
 
-// beforeEach(async () => {
-// 	// run the seeds programatically before each test to start fresh
-//     await db.seed.run()
-// })
 
 afterAll(async () => {
 	// close the database connection so the test process doesn't hang or give a warning
@@ -29,32 +25,32 @@ beforeEach((done) => {
     })
 })
 
-// describe('Test custom header', function() {
-//     it.only('sets header', function(done) {
-//       supertest(server)
-//         .get('/api/auth/querystring')
-//         .set("x-access-token", token)
-//         .expect(200)
-//         .end(function(err, res){
-//             if (err) return done(err);
-//             done();
-//         });
-//     });
-//   });
+describe('Test custom header', function() {
+    it.only('sets header', function(done) {
+      supertest(server)
+        .get('/api/auth/querystring')
+        .set("x-access-token", token)
+        .expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            done();
+        });
+    });
+  });
 
 describe('GET /api/jokes/', function() {
 
-    // it('should require authorization', async () => {
+    it('should require authorization', async () => {
         
-    //     const res = await supertest(server)
-    //         .get("/api/jokes")
-    //         .send({
-    //             username: 'user7',
-    //             password: 'pass7'
-    //         })
-    //     expect(res.statusCode).toBe(401)
-    //     expect(res.type).toBe("application/json")
-    // })
+        const res = await supertest(server)
+            .get("/api/jokes")
+            .send({
+                username: 'user7',
+                password: 'pass7'
+            })
+        expect(res.statusCode).toBe(401)
+        expect(res.type).toBe("application/json")
+    })
 
 
     it('should return json', function(done) {
@@ -73,31 +69,31 @@ describe('GET /api/jokes/', function() {
 
 });
 
-// describe('create user', function(){
+describe('create user', function(){
 
-//     it('should post register', async () => {
-// 		const res = await supertest(server)
-// 			.post("/api/auth/register")
-// 			.send({
-//                 username: 'user7',
-//                 password: 'pass7'
-//             })
-// 		expect(res.statusCode).toBe(201)
-// 		expect(res.type).toBe("application/json")
-//     })
-// })
+    it('should post register', async () => {
+		const res = await supertest(server)
+			.post("/api/auth/register")
+			.send({
+                username: 'user7',
+                password: 'pass7'
+            })
+		expect(res.statusCode).toBe(201)
+		expect(res.type).toBe("application/json")
+    })
+})
 
-// describe('sign in', function(){
-//     it ('should post to login', async () => {
-//         const res = await supertest(server)
-//             .post("/api/auth/login")
-//             .send({
-//                 username: 'user1',
-//                 password: 'pass100'
-//             })
-//         expect(res.statusCode).toBe(401)
-//         expect(res.type).toBe("application/json")
+describe('sign in', function(){
+    it ('should post to login', async () => {
+        const res = await supertest(server)
+            .post("/api/auth/login")
+            .send({
+                username: 'user1',
+                password: 'pass100'
+            })
+        expect(res.statusCode).toBe(401)
+        expect(res.type).toBe("application/json")
         
-//     })
-// })
+    })
+})
 
