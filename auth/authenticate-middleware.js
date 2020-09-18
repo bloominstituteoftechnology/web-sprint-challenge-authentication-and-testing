c9c42940056a6e13241788247bf9c17ac3e360aa
Stephen Gary <sgary0@protonmail.com>
2020-09-18 11:32:21 -0400

    auth

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,20 +1,8 @@
-const jwt = require("jsonwebtoken");
+/* 
+  complete the middleware code to check if the user is logged in
+  before granting access to the next middleware/route handler
+*/
 
 module.exports = (req, res, next) => {
-    const token = req.headers.authorization;
-    const secret = process.env.JWT_SECRET || ":)";
-
-    if (token) {
-        jwt.verify(token, secret, (err, decodedToken) => {
-            if (err) {
-                res.status(401).json({ message: "You shall not pass!" });
-            } else {
-                req.jwt = decodedToken;
-
-                next();
-            }
-        });
-    } else {
-        res.status(401).json({ message: "oops no token :(" });
-    }
+  res.status(401).json({ you: 'shall not pass!' });
 };
