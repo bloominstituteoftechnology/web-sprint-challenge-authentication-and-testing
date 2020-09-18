187a75dfc5a482374105c7356ae1daee08a01b9a
Ash <ashley.bergsma42@gmail.com>
2020-09-18 10:33:25 -0700

    Testing for auth router and jokes router are passing and failing as expected!

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -5,8 +5,17 @@ const server = require('../api/server');
 
 
 describe('Register User', () => {
+    // it('should kick back when not given a username or password', async() => {
+    //     const res = await request(server)
+    //         .post("api/auth/register")
+    //         .send({ username: "newNew" })
+    //     expect(res.status).toBe(401)
+    //     expect(res.body).toHaveProperty("message")
+    //     expect(res.type).toBe('application/json')
+    // })
+
     it ('should register users', async() => {
-        const newUser = {username: "gambino", password: "password"};
+        const newUser = {username: "markuser", password: "password"};
         const res = await request(server)
         .post("/api/auth/register")
         .send(newUser);
@@ -17,7 +26,16 @@ describe('Register User', () => {
 });
 
 describe('Login User', () => {
-    it('should login the user', async () => {
+    it('Should kickback an error for incorrect password', async () => {
+        
+        const res = await request(server)
+            .post('/api/auth/login')
+            .send({ username: "gambino", password: "incorrect" })
+        expect(res.status).toBe(403)
+        expect(res.type).toBe('application/json')
+    })
+
+    it('Should succeed with correct password', async () => {
         
         const res = await request(server)
             .post('/api/auth/login')
