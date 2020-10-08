f287c67e69ad3d904c09c2163d25d4f5b1ad1dfa
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-10-08 14:32:15 -0700

    broken

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,68 @@
+const bcryptjs = require('bcryptjs');
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/users-model');
+const { isValid } = require('../users/users-service');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const credentials = req.body;
+
+  try {
+    if (isValid(credentials)) {
+      const rounds = process.env.BCRYPT_ROUNDS ?
+        parseInt(process.env.BCRYPT_ROUNDS) : 8;
+
+      const hash = bcryptjs.hashSync(credentials.password, rounds);
+      credentials.password = hash;
+
+      const user = Users.add(credentials);
+      const token = generateToken(user);
+      res.status(201).json({ data: user, token });
+    } else {
+      res.status(400).json({message: 'username or password missing' });
+    }
+  } catch (err) {
+    res.status(500).json({message: 'error saving new user', ...err });
+  }
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const { username, password } = req.body;
+
+  try {
+
+    if (!isValid(req.body)) {
+      res.status(400).json({message: 'username or password missing' });
+    } else {
+      const [user] = Users.findBy({ username: username });
+      if (user && bcryptjs.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: 'welcome to the api', token: token });
+      } else {
+        res.status(401).json({message: 'invalid credentials' });
+      }
+    }
+  } catch (err) {
+    console.log("error", ...err)
+    res.status(500).json({message: 'db error logging in', ...err });
+  }
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+  
+  const secret = process.env.JWT_SECTRET || "tra-la-la";
+
+  const token = jwt.sign(payload, secret, options);
+  return token;
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,29 @@
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+  try{
+    const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : '';
+
+    if(token){
+      jwt.verify(token, secret, (err, decodedToken)=>{
+        if(err){
+          res.status(401).json({you: "shall not pass!"});
+        }else {
+          req.decodedToken = decodedToken;
+          next();
+        }
+      });
+    }else {
+      res.status(401).json({you: 'shall not pass!'});
+    }
+  } catch(err) {
+    res.status(500).json({message: 'error validating credentials', ...err});
+  }
+ 
 };
