e7cfc81f10282f7247c7a3538d41974c1da3bd3b
andrecolon <andrecolon@gmail.com>
2020-10-11 18:56:38 -0700

    Added testing dependencies

---
+++ api/server.spec.js
@@ -0,0 +1,10 @@
+//Unit tests - small parts of the code
+//Unit testing will be testing parts of the system that work in tandem
+//End to end tests - for endpoints
+
+
+//3 libraries in particular:
+//1. jest
+//2. supertest - http assertions library
+//3. Cross-env - NPM module that deal with OS inconsistencies acroess platforms
+
