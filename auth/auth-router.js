865b0adc341fe3bdbf8995630c944e06936961e7
andrecolon <andrecolon@gmail.com>
2020-10-11 18:23:46 -0700

    Added Regsitration

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
-
+const bcrypt = require("bcryptjs"); //step one 
+const Users = require("../user/user-model")
 router.post('/register', (req, res) => {
-  // implement registration
+  // implement registration with bcrypt
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+  Users.add(user)
+    .then((saved) => {
+      res.status(201).json(saved)
+    })
+    .catch(() => {
+      res.status(500).json(error)
+    })
 });
 
 router.post('/login', (req, res) => {
