93478e1a9fb437fdc379d5ec8e7e887785b1f821
andrecolon <andrecolon@gmail.com>
2020-10-11 18:31:28 -0700

    Added Login, somethings funny tho

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const bcrypt = require("bcryptjs"); //step one 
 const Users = require("../user/user-model")
+const jwt = require("jsonwebtoken")
+const { jwtSecret } = require("../config/secret")
+
 router.post('/register', (req, res) => {
   // implement registration with bcrypt
   let user = req.body;
@@ -16,7 +19,24 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  // implement login with JSON web tokens
+  let { username, password } = req.body
+  Users.findBy({ username })
+    .first()
+    .then((user) => {
+      if (user && bcrypt.hashSync(password), user.password) {
+        const token = generateToken(user);// get a token
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          token //send the token
+        })
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
 });
 
 module.exports = router;
---
+++ config/secret.js
@@ -0,0 +1,5 @@
+//what is the secret?
+
+module.exports = {
+    jwtSecret: process.env.JWT_SECRET || "It's a secret, never teach the Wutang!"
+}
