ec4d0aaeb0c4c4c905a55e81d30b51fbb49e7083
andrecolon <andrecolon@gmail.com>
2020-10-11 19:23:26 -0700

    2 running test - passed

--- api/server.spec.js
+++ api/server.spec.js
@@ -8,3 +8,27 @@
 //2. supertest - http assertions library
 //3. Cross-env - NPM module that deal with OS inconsistencies acroess platforms
 
+// You can avoid polluting the database by CREATING a new DB for testing
+
+const request = require('supertest');
+const server = require('./server');
+
+const db = require('../database/dbConfig');
+const { expectCt } = require('helmet');
+const { as } = require('../database/dbConfig');
+
+const testUser = { username: "sprinter", password: "testing"};
+
+describe('server.js', () =>{
+    describe("Get REQUEST for jokes", () =>{
+            it("should send a status code of 400 when not logged in", async () =>{
+                const res = await request(server).get("/api/jokes")
+                expect(res.status).toBe(400)
+            })
+            it("should return JSON", async () =>{
+                const res = await request(server).get("/api/jokes")
+                expect(res.type).toBe("application/json")
+            })
+    })
+
+})
