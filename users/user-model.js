870e88d4f9789a927612c83534c75b64f135c094
elbeg9amar <elbeg9amar@yahoo.com>
2020-10-16 11:31:57 -0400

    added register and login routers

--- users/user-model.js
+++ users/user-model.js
@@ -2,19 +2,25 @@ const db = require('../database/dbConfig');
 
 module.exports = {
     add,
-    getById
-}
+    getById,
+    getBy
+};
 
 async function add(body) {
     try {
         const [id] = await db("users").insert(body, "id");
-
         return getById(id);
     } catch (error) {
         throw error;
-    }
-}
+    };
+};
 
 function getById(id) {
     return db("users").where({ id }).first();
-}
+};
+
+function getBy(filter){
+    return db('users')
+        .where(filter)
+        .first()
+};
