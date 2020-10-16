e18866dbc69cd21e156dedab3b8e992dd11ed2ff
elbeg9amar <elbeg9amar@yahoo.com>
2020-10-16 12:12:33 -0400

    added auth for jokes API

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,21 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require("jsonwebtoken");
+
+const { jwtSecret } = require("../api/config.js");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+    const token = req.headers.authorization;
+  
+
+    if (token) {
+        jwt.verify(token, jwtSecret, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({ you: "Cant't touch this!" });
+            } else {
+                req.jwt = decodedToken;
+                next();
+            }
+        });
+    } else {
+        res.status(401).json({ you: "shall not pass!" });
+    }
 };
