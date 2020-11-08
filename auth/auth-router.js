53ec2ab9990cfae54a7f74bcddf89c4f08fec50e
Elizabeth Hagag <lizjanegiles@gmail.com>
2020-11-08 09:02:20 -0600

    added .env

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,72 @@
 const router = require('express').Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
+const User = require('./users-model')
+
+const {restrict} = require('./authenticate-middleware')
+
+const bcrypt = require('bcryptjs')
+
+const jwt = require('jsonwebtoken')
+
+
+router.post('/register', async (req, res, next) => {
+ try {
+   const { username, password} = req.body
+
+   const user = await User.findBy({username}).first()
+
+   if(user) {
+     return res.status(409).json({
+       message: "Username is Taken"
+     })
+   }
+
+   const addUser = await User.add({
+     username,
+     password: await bcrypt.hash(password, 14)
+   })
+
+   res.status(201).json(addUser)
+
+ } catch(err) {
+   next(err)
+ }
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  try {
+    const { username, password} = req.body
+    const user = await User.findBy({username}).first()
+
+    if (!user ) {
+      return res.status(401).json({
+        message: "Invalid User"
+      })
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password)
+
+    if(!validPassword) {
+      return res.status(401).json({
+        message: "Invalid Password"
+      })
+    }
+
+    //create new JSON toke with user details
+
+    const token = jwt.sign({
+      userID: user.id,
+      
+    })
+
+    req.session.user = user
+
+    res.json({
+      message: `Welcome ${user.username} !!!!`
+    })
+  } catch(err) {
+
+  }
 });
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,25 @@
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+const bcrypt = require('bcryptjs')
+const Users = require('./users-model')
+
+function restrict() {
+  return async (req,res,next) => {
+    try {
+
+      if(!req.session || !req.user ) {
+        return res.status(401).json({
+          message: "Invalid Credentials"
+        })
+      }
+
+    } catch(err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = {
+  restrict
+}
