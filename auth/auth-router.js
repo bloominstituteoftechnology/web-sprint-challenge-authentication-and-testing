0e977865dbf0caf9c1cfa3817340460e1dcb5c27
Steven Sussman <sjsussman@gmail.com>
2020-11-13 11:46:24 -0500

    completed routing, need tests

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,73 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { jwtSecret } = require('../database/secrets')
+const Users = require('../auth/auth-model')
 
 router.post('/register', (req, res) => {
   // implement registration
+  const credentials = req.body
+  if(credentials) {
+    const rounds = process.env.ROUNDS || 12;
+    const hash = bcrypt.hashSync(credentials.password, rounds)
+    credentials.password = hash;
+    Users.add(credentials)
+      .then(user => {
+        res.status(201).json({
+          data: user
+        })
+      })
+      .catch(error => {
+        res.json(500).json({
+          message: error.message
+        })
+      })
+
+  } else {
+    res.status(400).json({
+      message: "please provide a username and password that meets our guidelines"
+    })
+  }
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  const { username, password } = req.body;
+  if({ username, password }) {
+    Users.findBy({ username: username })
+    .then(([user]) => {
+        if(user && bcrypt.compareSync(password, user.password)) {
+            const token = createToken(user)
+            res.status(200).json({
+                message: 'You have successfully logged in', token //sends the token back in the response
+            })
+        } else {
+            res.status(401).json({
+                message: 'Invalid Credentials'
+            })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: error.message
+        })
+    })
+} else {
+    res.status(400).json({
+        message: ' please provide an exisiting username and password combination'
+    })
+}
 });
+
+function createToken(user) {
+  const payload = {
+      subject: user.id,
+      username: user.username,
+  }
+  const options = {
+      expiresIn: '60 seconds'
+  }
+  return jwt.sign(payload, jwtSecret, options)
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,24 @@
+const { jwtSecret } = require('../database/secrets')
+const jwt = require('jsonwebtoken')
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
-
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+  const token = req.headers.authorization
+  if(!token) {
+    res.status(401).json({
+      message: 'you are not authorized to view this page'
+      })
+    }
+    jwt.verify(token, jwtSecret, (error, decodedToken) => {
+      if(error) {
+        res.status(401).json({
+          message: 'token does not exist'
+        })
+      }
+      console.log('decoded token ->', decodedToken)
+      req.decodedJWT = decodedToken
+      next()
+    })
+}
