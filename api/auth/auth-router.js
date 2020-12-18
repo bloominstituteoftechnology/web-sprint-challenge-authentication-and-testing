7bda69cf87a40137bec236b1d18f914d8ac9c885
Ramsha <ramshanasir.rn@gmail.com>
2020-12-18 12:17:37 -0600

    MVP, finally

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,43 +2,47 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model');
 const jwt = require('jsonwebtoken');
-const {jwtSecret} = require('../auth/secret');
+const { jwtSecret } = require('../auth/secret');
+const { isValid } = require('../users/services');
+
 
 router.post('/register', (req, res) => {
-  let user = req.body
-  const hash = bcrypt.hashSync(user.password, 10)
-  // implement registration
-  user.password = hash;
-  Users.add(user)
-  .then((saved) => {
-    res.status(201).json(saved)
+  if (isValid(req.body)) {
+    let user = req.body
+    const hash = bcrypt.hashSync(user.password, 10)
+    // implement registration
+    user.password = hash;
+    Users.add(user)
+      .then((saved) => {
+        res.status(201).json(saved)
+      })
+      .catch((err) => {
+        res.status(500).json({ message: 'username taken' })
+      })
+  } else {
+    res.status(500).json({ message: 'username and password required' })
+  }
   })
-  .catch((err) => {
-    res.status(500).json({ message: 'username taken' });
-  });
-});
- 
 
+ 
 router.post('/login', (req, res) => {
   // implement login
   let { username, password } = req.body
-  Users.findBy({username})
-  .first()
-  .then((user) => {
-    if (user && bcrypt.compareSync(password, user.password)) {
-      const token = generateToken(user);
-      res.status(200).json({
-        message: `Welcome ${user.username}`,
-         token
-      });
-    } else {
-      res.status(401).json({message: 'invalid credentials'});
-    }
-  })
-  .catch((err) => {
-    res.status(500).json({message:'username and password required'})
-  })
-});
+  Users.findBy({ username })
+    .first()
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}`,token});
+      } else {
+        res.status(401).json({ message: 'invalid credentials' });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'username and password required' })
+    })  
+})
 
 function generateToken(user){
   const payload = {
@@ -47,7 +51,7 @@ function generateToken(user){
     lat: Date.now()
   }
   const options = {
-    expiresIn: '1h'
+    expiresIn: '20mins'
   }
   return jwt.sign(payload, jwtSecret, options)
 }
