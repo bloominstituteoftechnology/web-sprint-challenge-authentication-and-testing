41bc1ccbee0a5fbd2f44c8874dde26341117c7e8
Terry Edwards <terryedwardsjr113@gmail.com>
2021-01-24 23:47:11 -0500

    server up

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,37 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+const Users = require("./auth-model");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
+router.post('/register', async (req, res) => {
+  // res.end('implement register, please!');
+  try {
+		const { username, password} = req.body
+		const user = await Users.findBy({ username }).first()
+
+		if (user) {
+			return res.status(409).json({
+				message: "username taken",
+			})
+    }
+    
+    if(!username || password) {
+      return res.status(409).json({
+				message: "username and password required",
+			})
+    }
+
+		const newUser = await Users.add({
+			username,
+      password: await bcrypt.hash(password, 14),
+		})
+
+		res.status(201).json(newUser)
+	} catch(err) {
+		next(err)
+	}
+
+  
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -28,8 +58,37 @@ router.post('/register', (req, res) => {
   */
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+router.post('/login', async (req, res) => {
+  // res.end('implement login, please!');
+  try {
+		const { username, password } = req.body
+        
+    if (!username || !password) {
+			return res.status(401).json({
+				message: "username and password required",
+			})
+		}
+		const user = await Users.findBy({ username }).first()
+
+    const passwordValid = await bcrypt.compare(password, user.password)
+    
+		if (!user || !passwordValid) {
+			return res.status(401).json({
+				message: "invalid credentials",
+			})
+    }
+
+        const token = jwt.sign({
+            id: user.id,
+        }, process.env.JWT_SECRET)
+        
+		res.json({
+            message: `Welcome, ${user.username}!`,
+            token: token
+		})
+	} catch(err) {
+		next(err)
+	}
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,28 @@
-module.exports = (req, res, next) => {
-  next();
+ const jwt= require("jsonwebtoken")
+ 
+ module.exports =async (req, res, next) => {
+  
+  try {
+    const token= req.header.authorization
+    if(!token) {
+      return res.status(401).json({
+      message: "token required"
+      })
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=>{
+    if(err) {
+      return res.status(401).json({
+        message: "token invalid"
+      })
+    }
+    req.token= decoded
+
+    next();
+    })
+  }  catch(err) {
+    next(err)
+  }
+  
   /*
     IMPLEMENT
 
