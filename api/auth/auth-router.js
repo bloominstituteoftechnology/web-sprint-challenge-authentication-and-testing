20a5256a8017374e2298e03cb5032925b62597cd
Anam2091 <56008677+Anam2091@users.noreply.github.com>
2021-02-01 00:25:50 -0500

    sprint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const id = 0;
 
 router.post('/register', (req, res) => {
-  res.end('implement register, please!');
+  let user = req.body;
+  
+  if(user.username === undefined || user.password === undefined){
+    res.status(500).send("username and password required")
+  }
+  const hash = bcrypt.hashSync(user.password, 12);
+
+  user.password = hash;
+  
+  res.status(200).json({
+    ...user,
+    id: id + 1
+  })
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -29,7 +44,23 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+  let {username, password} = req.body;
+  
+  if(username === undefined || password === undefined){
+    res.status(500).send("username and password required")
+  }
+
+  Users.findBy({username: username}).first()
+  .then(user => {
+    if (user && bcrypt.compareSync(password, user.password)){
+      res.status(200).json({message: `welcome ${user.username}`})
+    }else {
+      res.status(401).json({message: 'Invalid Credentials'})
+    }
+  })
+  .catch(error => {
+    res.status(500).json(error);
+  })
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
