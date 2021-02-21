3521e705a4b4e2763c22ba8cbb15d13b3282a53f
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2021-02-21 14:34:25 -0800

    restricted middleware updated

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,26 @@
+const jwt = require("jsonwebtoken")
+const secrets = require('../../config/secrets.js')
+
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers?.authorization?.split(" ")[1] ?? req.headers?.authorization;
+
+  if(!token) {
+    res.status(403).json({message: "token required"});
+  } else {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if(err) {
+        res.status(403).json({message: "token not valid"});
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  }
+  
+  
+  
+
   /*
     IMPLEMENT
 
