7cb4cd83e88aafb4bd5c71f6ea252ae7b25a7ca0
Amanda Nelson <amanda.gaylord@gmail.com>
2021-03-21 19:54:08 -0400

    auth-router and server complete with dotenv values

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,35 +1,96 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const db = require("../../data/dbConfig");
+const secure = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
+//add new user:
+const dbAdd = async (user) => {
+  const [id] = await db("users").insert(user);
+  return dbFindById(id);
+};
+
+//find user from id:
+const dbFindById = (id) => {
+  return db("users").select("id", "username", "password").where({ id }).first();
+};
+
+//find user with filter:
+const dbFindByFilter = (filter) => {
+  return db("users").select("id", "username", "password").where(filter).first();
+};
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(406).json({
+        message: "username and password required",
+      });
+    }
+    const user = await dbFindByFilter({ username });
+    if (user) {
+      return res.status(409).json({
+        message: "This user already exists",
+      });
+    }
+    const newUser = await dbAdd({
+      username: username,
+      password: await secure.hash(password, process.env.HASH_LOOP),
+    });
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
 
-    1- In order to register a new account the client must provide `username` and `password`:
+    1- In order to register a new account the client must provide `username` and `password`: ✅
       {
         "username": "Captain Marvel", // must not exist already in the `users` table
         "password": "foobar"          // needs to be hashed before it's saved
       }
 
     2- On SUCCESSFUL registration,
-      the response body should have `id`, `username` and `password`:
+      the response body should have `id`, `username` and `password`: ✅
       {
         "id": 1,
         "username": "Captain Marvel",
         "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
       }
 
-    3- On FAILED registration due to `username` or `password` missing from the request body,
+    3- On FAILED registration due to `username` or `password` missing from the request body, ✅
       the response body should include a string exactly as follows: "username and password required".
 
-    4- On FAILED registration due to the `username` being taken,
+    4- On FAILED registration due to the `username` being taken, ✅
       the response body should include a string exactly as follows: "username taken".
   */
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  dbFindByFilter({ username })
+    .then((user) => {
+      if (user && secure.compareSync(password, user.password)) {
+        const token = jwt.sign(
+          {
+            userID: user.id,
+          },
+          process.env.JWT_SECRET
+        );
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          token: token,
+        });
+      } else {
+        res.status(401).json({
+          message: `invalid credentials`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
--- api/server.js
+++ api/server.js
@@ -1,19 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex");
+const database_access = require("../data/dbConfig");
+const environment = require("dotenv/config");
+const restrict = require("./middleware/restricted.js");
 
-const restrict = require('./middleware/restricted.js');
-
-const authRouter = require('./auth/auth-router.js');
-const jokesRouter = require('./jokes/jokes-router.js');
+const authRouter = require("./auth/auth-router.js");
+const jokesRouter = require("./jokes/jokes-router.js");
+const Knex = require("knex");
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
+server.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.JWT_VERIFY || "shhh",
+    store: new KnexSessionStore({
+      knex: database_access,
+      createtable: true,
+    }),
+  })
+);
+server.use("/api/auth", authRouter);
+server.use("/api/jokes", restrict, jokesRouter); // only logged-in users should have access!
 
 module.exports = server;
