ab5e69a40bf00a2de3e435c9062570c262dcf988
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-04-18 19:08:38 -0600

    server update

--- api/server.js
+++ api/server.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
@@ -15,5 +17,13 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
+
+server.use((err, req, res, next) => {
+	console.log(err);
+	console.log(req.body);
+	res.status(500).json({
+		message: 'Something went wrong',
+	});
+});
 
 module.exports = server;
