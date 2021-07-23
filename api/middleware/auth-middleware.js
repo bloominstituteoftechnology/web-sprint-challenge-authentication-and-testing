ca34702d72e6cfcdc13edf8b559bde27893cc142
Vehmeyer <j.vehmeyer@gmail.com>
2021-07-23 12:29:31 -0500

    wip

--- api/middleware/auth-middleware.js
+++ api/middleware/auth-middleware.js
@@ -1,5 +1,5 @@
-const { jwtSecret } = require('../../config/secrets');
-const jwt = require('jsonwebtoken');
+// const { jwtSecret } = require('../../config/secrets');
+// const jwt = require('jsonwebtoken');
 const Users = require('../users/users-model');
 
 // On FAILED registration or login due to `username` or `password` missing from the request body,
@@ -24,7 +24,7 @@ const checkPayload = (req, res, next) => {
 //       the response body should include a string exactly as follows: "username taken".
 const uniqueUsername = async (req, res, next) => {
     try {
-        const existingUsername = await Users.getByUsername(req.body.username)
+        const existingUsername = await Users.findByUsername(req.body.username)
         if (existingUsername) {
             next({status: 400, message: 'username taken'})
         } else {
@@ -40,7 +40,9 @@ const uniqueUsername = async (req, res, next) => {
 const checkLoginPayload = async (req, res, next) => {
     try {
         // const { username, password } = req.body
-        const user = await Users.getByUsername(req.body.username)
+        const user = await Users.findByUsername(req.body.username)
+        // need to decode hash for comparison, 
+        // or need to convert inputted password to hash before calling db
         const password = await Users.validatePassword(req.body.password)
         if (!user || !password) {
             next({status: 400, massage: 'invalid credentials'})
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,22 @@
+const request = require('supertest');
+const db = require('../data/dbConfig');
+const server = require('./server');
+
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
+
+test('is the correct environment', () => {
+  expect(process.env.DB_ENV).toBe('testing')
+})
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+}) // migrate
+beforeEach(async () => {
+  await db.seed.run()
+}) // truncate and seed fresh data
+afterAll(async () => {
+  await db.destroy()
+}) // disconnect from the db
