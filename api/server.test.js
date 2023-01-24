cb50f127d30c3a5e6f60a0f713d56179f0dcf007
Terri Archer <territangela@gmail.com>
2023-01-24 18:54:10 -0500

    corrected spelling

--- api/server.test.js
+++ api/server.test.js
@@ -20,7 +20,7 @@ describe('[GET] /jokes', () => {
     await request(server).post('/api/auth/register').send(newUser)
     await request(server).post('/api/auth/login').send(newUser)
     const data = await request(server).get('/api/jokes')
-    expect(data.body.message).toBe('endpoint restricted')
+    expect(data.body.message).toBe('token required')
   })
   test('returns a list of jokes while authorized', async () => {
     await request(server).post('/api/auth/register').send(newUser)
