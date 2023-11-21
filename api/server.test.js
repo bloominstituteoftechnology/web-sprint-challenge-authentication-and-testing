3041c74d3f8908f8c63708b8665b618fc9e6723c
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-21 11:41:59 -0500

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -19,12 +19,26 @@ test('sanity', () => {
   expect(true).toBe(false)
 })
 
-// describe('[GET] /jokes', () => {
-//   test('a token exists', async () => {
-//     const jokes = await request(server).get('./jokes/jokes-router.js')
-//     expect(jokes).toBeDefined()
-//     expect(jokes).toHaveLength()
-//   })
-// })
+describe('[POST] /api/auth/register', () => {
+  test('gets success status', async () => {
+    const credentials = {username: 'hello', password: '1234'}
+    const res = await request(server).post('/api/auth/register').send(credentials)
+    expect(res.status).toBe(201)
+  })
+  test('adds credentials to database', async () => {
+    const creds = {username: 'hello', password: '1234'}
+    await request(server).post('/api/auth/register').send(creds)
+    const exists = await db('users').select('username').where('username', creds.username)
+    expect(exists).toBeTruthy()
+  })
+})
+
+describe('[GET] /jokes', () => {
+  test('a token exists', async () => {
+    const jokes = await request(server).get('./jokes/jokes-router.js')
+    expect(jokes).toBeDefined()
+    expect(jokes).toHaveLength()
+  })
+})
 
 
