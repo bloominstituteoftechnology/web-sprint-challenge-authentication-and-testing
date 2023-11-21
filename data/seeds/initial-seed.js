8c9c78f3018975d93f30577a36fc6615137e984f
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-21 10:35:03 -0500

    seed update

--- data/seeds/initial-seed.js
+++ data/seeds/initial-seed.js
@@ -1,13 +1,7 @@
 const jokes = require('../../api/jokes/jokes-data')
-const bcrypt = require('bcryptjs')
-
-const password = bcrypt.hashSync('1234', 8)
 
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('jokes').truncate()
   await knex('jokes').insert(jokes);
-
-  await knex('users').truncate()
-  await knex('users').insert({username: 'hello', password: password})
 };
