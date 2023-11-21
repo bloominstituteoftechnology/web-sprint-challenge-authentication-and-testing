5c7b5dbb43effc9a8508a5d0edec617194f3163b
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-21 10:29:17 -0500

    seed and migrate

--- data/migrations/20201123181212_users.js
+++ data/migrations/20201123181212_users.js
@@ -1,12 +1,19 @@
-exports.up = function (knex) {
-  return knex.schema.createTable('users', users => {
-    users.increments();
-    users.string('username', 255).notNullable().unique();
-    users.string('password', 255).notNullable();
-  });
+exports.up = async function (knex) {
+
+    await knex.schema.createTable('users', users => {
+      users.increments();
+      users.string('username', 255).notNullable().unique();
+      users.string('password', 255).notNullable();
+    });
+
+    await knex.schema.createTable('jokes', table => {
+      table.increments('db_id')
+      table.string('id').notNullable()
+      table.string('joke', 400).notNullable().unique()
+    })
 };
 
-exports.down = function (knex) {
-  return knex.schema.dropTableIfExists('users');
-  
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('users');
+  await knex.schema.dropTableIfExists('jokes');
 };
